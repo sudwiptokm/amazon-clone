0fb8bf54afd585273936d3957d7faf011f83b672
sudwiptokm <sudwiptokmondal@gmail.com>
2022-11-07 11:29:00 +0600

    save

--- src/components/Checkout/RightCheckout.js
+++ src/components/Checkout/RightCheckout.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectItems } from "../../slices/basketSlice";
 
 export default function RightCheckout() {
-  return <div></div>;
+  const items = useSelector(selectItems);
+  return (
+    <div>
+      {items.length > 0 && (
+        <>
+          <h2>Subtotal ({items.length} items):
+          <span className="font-bold">
+            
+          </span>
+          </h2>
+        </>
+      )}
+    </div>
+  );
 }
